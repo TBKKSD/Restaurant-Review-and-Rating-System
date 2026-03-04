6ac3cfbaab649801e1fc00b5f8ea4e16b6e88291
Kitsadee Thongbankao <kitsadee16@gmial.com>
2026-03-04 22:59:52 +0700

    Update db.js server.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,8 +1,30 @@
-import mysql from 'mysql2/promise'
+const mysql = require('mysql2')
+const mongoose = require('mongoose');
+const e = require('express');
+require('dotenv').config()
 
-export const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'rrrs_db'
-})
+const db = mysql.createPool({
+  host: process.env.MYSQL_HOST || 'mysql',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+  database: process.env.MYSQL_NAME || 'rrrs_db',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+const connectMongoDB = async () => {
+  try {
+    const mongoURI = process.env.MONGO_URI || 'mongodb://mongo_db:27017/rrrs_reviews';
+    await mongoose.connect(mongoURI);
+    console.log('Connected to MongoDB');
+  }catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = {
+    mysqlPool,
+    connectMongoDB
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,26 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+const express = require('express');
+const { connectMongoDB } = require('./db');
+require('dotenv').config();
 
-dotenv.config()
-
-const app = express()
-app.use(cors())
+const app = express();
 app.use(express.json())
 
+connectMongoDB();
+
 app.get('/api/health', (req, res) => {
-  res.json({ message: 'RRRS API running' })
-})
+    res.json({ status: 'ok' });
+});
 
-const PORT = process.env.PORT || 5000
+app.get('/test-db', async (req, res) => {
+    try {
+        const [rows] = await require('./db').mysqlPool.query('SELECT 1 + 1 AS result');
+        res.json({ message: 'MySQL is working!', data: rows });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`🚀 Server running on port ${PORT}`);
+});
