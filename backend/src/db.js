af20dc24a62391d60614294083a9bde3298e81d1
Peranut Tasanavinyu <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>
2026-04-04 00:15:37 +0700

    ADD for deploy back and db

--- backend/src/db.js
+++ backend/src/db.js
@@ -5,6 +5,11 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "rootpassword",
   database: process.env.DB_NAME || "rrrs_db",
+  port: process.env.DB_PORT || 4000,
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true // TiDB works with default Node.js certs
+  },
   waitForConnections: true,
   connectionLimit: 10,
 });
